363f6d50811492266755def487c87e031eff23ba
Vaynenoir <iv.kazadaev21@yandex.ru>
2017-10-09 18:22:44 +0300

    smooth scrolling, fixing the path-stay bug

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-var path = document.querySelector('.st0');
-var length = path.getTotalLength(200,157);
-	var p = path.getPointAtLength(200,157);
-
-
-var circle = document.querySelector('#movingCircle');
-console.log(circle);
-		console.log(p);
-  function inWindow(s){
+ function inWindow(s){
     if(!$(s).length) return false; // element not found
 
 		var img_top_offset = $(s).offset().top;
@@ -16,9 +8,17 @@ console.log(circle);
    
     return (bottomOfScreen > img_top_offset) && (topOfScreen < img_bottom);
 }
+$(document).ready(function() {
+//Chrome Smooth Scroll
+
+	// Default
+	jQuery.scrollSpeed(100, 1200);
+
+	// Custom Easing
 
 
 
+});
 
   		var path1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
   		$(path1).css('display','none');
@@ -38,8 +38,8 @@ function sectionCounter(){
 		if(topOffset <= scrollCount){
 
 				$(this).addClass("active");
-				$('.bg_svg').stop(400).animate({zoom: dataZoom, left: dataPos},400);
-				$('.st2').stop(200).fadeIn(400);
+				$('.bg_svg').stop(400).animate({zoom: dataZoom, left: dataPos},200);
+				$('.st2').stop(200).fadeIn(200);
 				$('.path_circle_2').stop().fadeIn(400);
 				$('.path_circle_3').stop().fadeIn(400);
 
@@ -63,37 +63,50 @@ $('img').each(function(el){
 	arrImgID.push($(this));
 
 });
+	var vis_count = 0;
 	// console.log(arrImgID);
 	$(arrImgID).each(function(el,i){
 
 
-
-
 			if(inWindow($(this))){
-		var cx = $(this).attr('data-pos-x');
-		var cy = $(this).attr('data-pos-y');
-		var Coordinates = $(this)[0].getBoundingClientRect();
+				vis_count++;
+				var cx = $(this).attr('data-pos-x');
+				var cy = $(this).attr('data-pos-y');
+				var Coordinates = $(this)[0].getBoundingClientRect();
+				
+				// console.log("Top:"+Coordinates.top+", Left:"+Coordinates.left+", Right:"+Coordinates.right+", Bottom:"+Coordinates.bottom);
+				var bottomOffsetImg = Coordinates.top + $(this).height();
+				// console.log(Coordinates);
+				$(path1).attr('d', "M"+cx + " " + cy +  " L"+ (-Coordinates.left) + " " + (Coordinates.top) +  " L" + (-Coordinates.left) + " " + (Coordinates.bottom -30) );
+
+				$(path1).css('position',"relative");
+				$(path1).attr('fill', '#47DBB4');
+				$(path1).attr('opacity', '.1');
+
+				//$(path1).fadeIn(900);
+		}
+		//else if(!inWindow($('img'))) {	
+
+				//$(path1).fadeOut(900);
+
+		//}
+
 		
-		// console.log("Top:"+Coordinates.top+", Left:"+Coordinates.left+", Right:"+Coordinates.right+", Bottom:"+Coordinates.bottom);
-		var bottomOffsetImg = Coordinates.top + $(this).height();
-		console.log(Coordinates);
-		$(path1).attr('d', "M"+cx + " " + cy +  " L"+ (-Coordinates.left) + " " + (Coordinates.top) +  " L" + (-Coordinates.left) + " " + (Coordinates.bottom -30) );
 
-		$(path1).css('position',"relative");
-		$(path1).attr('fill', '#26a69a');
-		$(path1).attr('opacity', '.1');
-		$(path1).fadeIn(900);
-}
-else {	
-
-		// $(path1).fadeOut(900);
-
-}
 // if(!(inWindow($('.js-image1'))))
 // else if(!(inWindow($('.js-image2')))){
 // 	$(path1).fadeOut(900);
 // }
 });
+
+		if (vis_count > 0) {
+			$(path1).fadeIn(900);
+		}
+		else {
+			$(path1).fadeOut(900);
+		}
+
+
 }
 
 
@@ -102,39 +115,41 @@ else {
 
 
 $(document).ready(function(){
+		var path = document.querySelector('.st0');
+		var length = path.getTotalLength();
 		$('.bg_svg svg').append(path1);
 		$('.bg_svg svg').append(path2);
 	var $dashOffset = $('.st0').css("stroke-dashoffset");
-			var offsetDash = $('.st0').css("stroke-dashoffset");
-	$(window).scroll(function(){
-		console.log("img 1 = "+inWindow('.js-image1'));
-		console.log("img 2 = "+inWindow('.js-image2'));
+			var offsetDash = $('.st0').css({
+				"stroke-dashoffset": length,
+				"stroke-dasharray": length + " " + length
+			});
+
+
+
+
+
+	$(window).scroll(function lol(){
+		// console.log("img 1 = "+inWindow('.js-image1'));
+		// console.log("img 2 = "+inWindow('.js-image2'));
 		var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())) * 100);
 
 
 
-		var $newUnit = parseInt($dashOffset, 10);
-		var $offsetUnit = $percentageComplete * ($newUnit / 100);
-		$('.st0').css("stroke-dashoffset", $newUnit - $offsetUnit);
 
+		var $newUnit = length;//parseInt($dashOffset, 10);
+		var $offsetUnit = $percentageComplete * ($newUnit / 200);
+		var	offsetCounter = $newUnit - ($offsetUnit); 
+	
+	if (offsetCounter < 2220 && offsetCounter>2190) return $('.st0').stop(2000).animate({"stroke-dashoffset": "2215px"},2000);
+	else $('.st0').css("stroke-dashoffset", $newUnit - ($offsetUnit));
+
+	
 		sectionCounter();			
 		
-				
-
-
-				if(Math.round($newUnit - $offsetUnit) < 830 || $newUnit - $offsetUnit == 723 || $newUnit - $offsetUnit == 490){
-					$('.path_circle_1').css("fill", "#000");
-				}
-				if(Math.round($newUnit - $offsetUnit) < 723){
-					$('.path_circle_2').css("fill","#000");
-				}
-				if(Math.round($newUnit - $offsetUnit) < 490){
-					$('.path_circle_3').css("fill", "#000");
-				}
-
-
 
 	});
+
 });
 
 
