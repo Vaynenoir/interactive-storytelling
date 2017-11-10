a5b89c08350cb7d149956766aa7e1b4ef7a58af7
Vaynenoir <iv.kazadaev21@yandex.ru>
2017-11-10 20:56:15 +0200

    drawing path light to img from point, drawing path, fixed zoom and displacement

--- pen_tool/ckeditor/build-config.js
+++ pen_tool/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/8c544a83fa4f9df14c79ca22279ee360
+ * (2) http://ckeditor.com/builder/59eb95ed5d4ebb768de02687ff7c67b2
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/8c544a83fa4f9df14c79ca22279ee360
+ * (3) http://ckeditor.com/builder/download/59eb95ed5d4ebb768de02687ff7c67b2
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -54,18 +54,19 @@ var CKBUILDER_CONFIG = {
 		'basicstyles' : 1,
 		'blockquote' : 1,
 		'clipboard' : 1,
-		'colordialog' : 1,
+		'colorbutton' : 1,
 		'contextmenu' : 1,
+		'div' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
 		'filebrowser' : 1,
 		'floatingspace' : 1,
-		'font' : 1,
 		'format' : 1,
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
 		'image' : 1,
+		'imageuploader' : 1,
 		'indentlist' : 1,
 		'link' : 1,
 		'list' : 1,
@@ -75,6 +76,7 @@ var CKBUILDER_CONFIG = {
 		'pastetext' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
+		'scayt' : 1,
 		'showborders' : 1,
 		'sourcearea' : 1,
 		'specialchar' : 1,
@@ -84,9 +86,7 @@ var CKBUILDER_CONFIG = {
 		'tableselection' : 1,
 		'tabletools' : 1,
 		'toolbar' : 1,
-		'uicolor' : 1,
 		'undo' : 1,
-		'uploadimage' : 1,
 		'wysiwygarea' : 1
 	},
 	languages : {
--- pen_tool/ready_page/js/rp_script.js
+++ pen_tool/ready_page/js/rp_script.js
@@ -1,17 +1,60 @@
 $(document).ready(function(){
-	     var dataContent = JSON.parse(localStorage.getItem('PointsContent'));
+
+	   var dataContent = JSON.parse(localStorage.getItem('PointsContent'));
      console.log(dataContent);
+
+     function inWindow(s) {
+     		if (!$(s).length) return false; // element not found
+
+    		 var img_top_offset = $(s).offset().top;
+    		 var img_bottom = $(s).offset().top + $(s).outerHeight();
+    		 var bottomOfScreen = $(window).scrollTop() + $(window).height();
+    		 var topOfScreen = $(window).scrollTop();
+
+    		 return (bottomOfScreen > img_top_offset) && (topOfScreen < img_bottom);
+ 		}
+
+
+
+
+     
+
+
      function compareId(objA, objB) {
  			 return objA.pointId - objB.pointId;
 			}
-     dataContent.sort(compareId);
-     var text_cont = document.getElementById("textWrap");
-     for(var i=0;i<dataContent.length;i++){
-     	text_cont.insertAdjacentHTML('beforeend', dataContent[i].data); ;
-     	
-     }
-     $('#textWrap p').addClass('kek');
 
+
+     dataContent.sort(compareId);
+     var text_cont = document.getElementById("boom");
+
+     		for(var i=0;i<dataContent.length;i++){
+     			var section = document.createElement('section');
+     			$(section).addClass('js-section');
+     			$(section).attr({
+     				"data-zoom": dataContent[i].zoom + "%",
+     				"data-pos-top": dataContent[i].top  + "%",
+     				"data-pos-bottom": dataContent[i].bottom  + "%",
+     				"data-pos-left": dataContent[i].left  + "%",
+     				"data-pos-right": dataContent[i].bottom  + "%"
+     		});
+
+
+     			section.insertAdjacentHTML('beforeend', dataContent[i].data);
+     			$(section).find('img').addClass('js-image');
+     			$(section).find('.js-image').attr('data-pos-x', 0);
+     			$(section).find('.js-image').attr('data-pos-y', 0);
+
+
+     			text_cont.append(section);	
+     	}
+
+
+
+     // $('#textWrap p').addClass('kek');
+$('.mapbg').css("zoom", "0%");
+$('.mapbg').css("right", "0");
+$('mapbg').css("bottom", "0");
  var a = document.getElementById('map');
 
     var map_url = JSON.parse(localStorage.getItem('map'));
@@ -38,6 +81,149 @@ $(document).ready(function(){
 
             // console.log(path);
         }
+
+
+
+
+
+     	   var currentDisplacementTop = parseInt($("#IDmapbg")[0].style.top);
+     	   var currentDisplacementLeft = parseInt($('#IDmapbg')[0].style.left);
+         // console.log(currentDisplacementLeft);
+          var path1 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+ 					$(path1).css('display', 'none');
+
+
+function sectionCounter() {
+     $('.js-section').each(function() {
+
+         var scrollCount = $(window).scrollTop();
+         var topOffset = $(this).offset().top;
+         var dataZoom = $(this).attr("data-zoom");
+         // console.log(dataZoom);
+         var dataPosTop = parseInt($(this).attr("data-pos-top")) + currentDisplacementTop + "%" ;
+         var dataPosBottom = $(this).attr('data-pos-bottom');
+         var dataPosLeft = parseInt($(this).attr("data-pos-left")) + currentDisplacementLeft + "%";
+         var dataPosRight = $(this).attr("data-pos-right");
+
+
+         // console.log(dataPosLeft);
+         if (topOffset <= scrollCount) {
+
+             $(this).addClass("active");
+             
+             $('.mapbg').stop(400).animate({
+                 zoom: dataZoom,
+                 left: dataPosLeft ,
+                 right: dataPosRight,
+                 top: dataPosTop,
+                 bottom: dataPosBottom
+             }, 400);
+   
+
+
+         } else {
+             $(this).removeClass("active");
+             if (!$('.js-section').hasClass("active")) {
+                 $('.mapbg').stop().animate({
+                     left: currentDisplacementLeft + "%",
+                     top: currentDisplacementTop + "%",
+                     zoom: "100%"
+                 }, 400);
+
+             }
+         }
+
+     });
+
+     var arrImgID = [];
+     $('.js-image').each(function(el) {
+
+         arrImgID.push($(this));
+     });
+
+
+    var CirclePathCoords = JSON.parse(localStorage.getItem('circlesCoords'));
+    $(arrImgID).each(function(i,el){
+     		$(this).attr({
+     			'data-pos-x': CirclePathCoords[i].cx,
+     			'data-pos-y': CirclePathCoords[i].cy
+     });
+     		console.log(el);
+     	}); 
+     // var vis_count = 0;
+     console.log(arrImgID);
+
+
+     var vis_count = 0;
+
+
+
+
+
+     $(arrImgID).each(function() {
+         if (inWindow($(this))) {
+             vis_count++;
+        //      	console.log($(this));
+ 							// console.log($(this));
+             	 	var cx = $(this).attr('data-pos-x');
+             		var cy = $(this).attr('data-pos-y');
+             		// console.log(circle);
+             		
+
+
+             var Coordinates = $(this)[0].getBoundingClientRect();
+
+             // console.log("Top:"+Coordinates.top+", Left:"+Coordinates.left+", Right:"+Coordinates.right+", Bottom:"+Coordinates.bottom);
+             var bottomOffsetImg = Coordinates.top + $(this).height();
+             // console.log(Coordinates);
+             $(path1).attr('d', "M" + cx + " " + cy + " L" + (-Coordinates.left) + " " + (Coordinates.top-120) + " L" + (Coordinates.left) + " " + (Coordinates.bottom-200));
+             $(path1).css('position', "relative");
+             $(path1).attr('fill', '#47DBB4');
+             $(path1).attr('opacity', '.2');
+
+         }
+     });
+
+
+svgRoot.append(path1);
+     if (vis_count > 0) {
+         $(path1).fadeIn(900);
+     } else {
+         $(path1).fadeOut(900);
+     }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -86,11 +272,136 @@ $(document).ready(function(){
      	text_cont.style.display = "block";
 
 
+
+
+
+
+
+
+
+     	var length = path.getTotalLength();
+
+
+     var offsetDash = $(path).css({
+         "stroke-dashoffset": length,
+         "stroke-dasharray": length + " " + length
+     });
+     
+     var StopPoints = JSON.parse(localStorage.getItem('stopsAtLength'));
+     // for(var i=0;i<StopPoints.length;i++){
+     // 	var stopPoint = StopPoints[i];
+     // 	console.log(stopPoint);
+     // }
+     var stops = (function(pathLength){
+     	for(var i=0; i< StopPoints.length;i++){
+     		var stopPoint = StopPoints[i];
+     		// console.log(pathLength);
+     		if((stopPoint - 8)<= pathLength <=(stopPoint)){
+     			// console.log(pathLength);
+     		}
+     	}
+     });
+     
+
+var  counter = 0;
+// console.log(length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function scrolled(e) {
+
+
+	   var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())) * 100);
+
+
+	   // console.log(pathBegin);
+	   	// console.log($(path).css("stroke-dashoffset"));
+	   
+
+    var $newUnit = length; //parseInt($dashOffset, 10);
+    var $offsetUnit = $percentageComplete * ($newUnit / 500);
+    var offsetCounter = $newUnit - ($offsetUnit);
+    // console.log(Math.round($offsetUnit));
+    // console.log(path.pathLength);
+ 
+$(path).css("stroke-dashoffset", ($newUnit - ($offsetUnit)));
+    // console.log(Math.floor($offsetUnit));
+    
+    if(20>($newUnit - ($offsetUnit)) >0){
+    	$(path).css("stroke-dashoffset", "0px");
+    }
+
+	sectionCounter();
+
+	for(var i=0; i< StopPoints.length;i++){
+		   
+				var faultLess = StopPoints[i] - 6;
+				var faultMore = StopPoints[i] + 20;
+				var pathCurrentLength = Math.floor($offsetUnit);
+				// console.log(faultMore,faultLess);
+     		if( pathCurrentLength < faultMore && pathCurrentLength > faultLess){
+     		// $(path).css("stroke-dashoffset", "" +($newUnit - StopPoints[i]) + "px");
+     		// $(window).off("scroll", scrolled);
+     		
+
+
+				// $(window).on("scroll resize");
+     	}
+     	// else if(pathCurrentLength > faultMore && pathCurrentLength < faultLess ){
+     	// 	$(window).on("scroll");
+     	// 	$(path).css("stroke-dashoffset", ($newUnit - ($offsetUnit)));
+     	// }
+     	
+     	// else{
+     	// 	$(window).bind("scroll", function(){
+      //       scroll();
+      //   });
+     	// 	}
+     	// else if(pathCurrentLength > faultMore && pathCurrentLength < faultLess ){
+     	// 	$(path).css("stroke-dashoffset", ($newUnit - ($offsetUnit)));
+     	// }
+     	}	
+}
+
+
+
+$(window).on("scroll", scrolled);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       }, false);
+
      var WrapperProps = JSON.parse(localStorage.getItem('mapStyleProperties'));
     console.log(WrapperProps);
     $.each(WrapperProps, function(prop, value) {
-        $(".mapbg").css(prop, value);
+        $("#IDmapbg").css(prop, value);
     });
 
 });
